b4975eb39ea01464e3c8ebd1e06fef7cda647262
YehyeokBang <qkddpgur318@gmail.com>
2023-03-22 15:23:50 +0900

    feat: init project

---
+++ src/main/java/skhu/easysobi/EasySobiApplication.java
@@ -0,0 +1,13 @@
+package skhu.easysobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySobiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasySobiApplication.class, args);
+    }
+
+}
