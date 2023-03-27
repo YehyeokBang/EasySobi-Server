fc816fb1ea5320af08bde0d024ccc5dc23271dd0
YehyeokBang <qkddpgur318@gmail.com>
2023-03-27 15:52:07 +0900

    feat: Add JpaAuditing

--- src/main/java/skhu/easysobi/EasySobiApplication.java
+++ src/main/java/skhu/easysobi/EasySobiApplication.java
@@ -2,9 +2,11 @@ package skhu.easysobi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
-@SpringBootApplication()
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EasySobiApplication {
 
     public static void main(String[] args) {
