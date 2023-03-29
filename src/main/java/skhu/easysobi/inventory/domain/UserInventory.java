1de972a7d91962c45c278727ad6a92bb9b519279
YehyeokBang <qkddpgur318@gmail.com>
2023-03-30 03:00:53 +0900

    #4 feat: Add accessStatus column in UserInventory Entity

--- src/main/java/skhu/easysobi/inventory/domain/UserInventory.java
+++ src/main/java/skhu/easysobi/inventory/domain/UserInventory.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import skhu.easysobi.auth.domain.User;
 import skhu.easysobi.inventory.dto.UserInventoryDTO;
 
 @Entity
 @Builder
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserInventory {
@@ -24,6 +26,9 @@ public class UserInventory {
     @ManyToOne
     @JoinColumn(name = "inventory_id")
     private Inventory inventory;
+
+    @Builder.Default
+    private Boolean accessStatus = true; // 접근 상태
 
     public UserInventoryDTO.Response toResponseDTO() {
         return UserInventoryDTO.Response.builder()
