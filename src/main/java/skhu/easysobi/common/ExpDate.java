37ba6ad6f2e2d5a8f539b5f0a7c658ee08c948d8
YehyeokBang <qkddpgur318@gmail.com>
2023-04-06 14:06:41 +0900

    feat: Add ExpDate Calculator

---
+++ src/main/java/skhu/easysobi/common/ExpDate.java
@@ -0,0 +1,30 @@
+package skhu.easysobi.common;
+
+import lombok.RequiredArgsConstructor;
+import skhu.easysobi.inventory.domain.Category;
+import skhu.easysobi.inventory.repository.CategoryRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ExpDate {
+
+    // 소비기한 계산하는 메서드
+    public static LocalDateTime calcExpDate(LocalDateTime mfgDate, CategoryRepository categoryRepository, Long categoryNum) {
+        Optional<Category> optionalCategory = categoryRepository.findByCategoryNum(categoryNum);
+
+        // 카테고리가 존재하는 경우
+        if (optionalCategory.isPresent()) {
+            // 해당 카테고리의 소비기한 일수를 알아내어 제조일자에 더한 후
+            // 해당 아이템의 소비기한 설정하기
+            Category category = optionalCategory.get();
+            int exp = category.getExp();
+
+            // 소비기한 구하기
+            return mfgDate.plusDays(exp);
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+}
--- src/main/java/skhu/easysobi/inventory/service/ItemService.java
+++ src/main/java/skhu/easysobi/inventory/service/ItemService.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Service;
 import skhu.easysobi.inventory.domain.Inventory;
 import skhu.easysobi.inventory.domain.Item;
 import skhu.easysobi.inventory.dto.ItemDTO;
+import skhu.easysobi.inventory.repository.CategoryRepository;
 import skhu.easysobi.inventory.repository.InventoryRepository;
 import skhu.easysobi.inventory.repository.ItemRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+
+import static skhu.easysobi.common.ExpDate.calcExpDate;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
     private final InventoryRepository inventoryRepository;
+    private final CategoryRepository categoryRepository;
 
     // 아이템 조회
     public ItemDTO.ResponseItem findItemById(Long id) {
@@ -29,6 +34,12 @@ public class ItemService {
 
         if (optionalInventory.isPresent()) {
             dto.setInventory(optionalInventory.get());
+
+            // 아이템의 소비기한 설정
+            LocalDateTime expDate = calcExpDate(dto.getMfgDate(), categoryRepository, dto.getCategoryNum());
+            System.out.println(expDate);
+            dto.setExpDate(expDate);
+
             itemRepository.save(dto.toEntity());
         }
     }
@@ -42,8 +53,12 @@ public class ItemService {
         if (optionalItem.isPresent()) {
             Item item = optionalItem.get();
 
+            // 아이템의 소비기한 설정
+            LocalDateTime expDate = calcExpDate(dto.getMfgDate(), categoryRepository, dto.getCategoryNum());
+            dto.setExpDate(expDate);
+
             // 아이템 수정
-            item.updateItem(dto.getName(), dto.getCategory(), dto.getCount(), dto.getMfgDate());
+            item.updateItem(dto.getName(), dto.getCategoryNum(), dto.getCount(), dto.getMfgDate(), dto.getExpDate());
             itemRepository.save(item);
         }
     }
