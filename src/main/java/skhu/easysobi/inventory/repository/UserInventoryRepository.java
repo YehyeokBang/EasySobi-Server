446a9bb10e4dac42ff77bfe457a0d11ae7ec41af
YehyeokBang <qkddpgur318@gmail.com>
2023-05-31 16:55:58 +0900

    feat: Add findAllByExpiredItemsInInventory method (#16)

--- src/main/java/skhu/easysobi/inventory/repository/UserInventoryRepository.java
+++ src/main/java/skhu/easysobi/inventory/repository/UserInventoryRepository.java
@@ -1,6 +1,7 @@
 package skhu.easysobi.inventory.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import skhu.easysobi.inventory.domain.Inventory;
 import skhu.easysobi.inventory.domain.UserInventory;
 
@@ -17,5 +18,8 @@ public interface UserInventoryRepository extends JpaRepository<UserInventory, Lo
 
     // id로 접근 권한 찾기
     Optional<UserInventory> findByInventoryId(Long id);
+
+    @Query("SELECT ui FROM UserInventory ui JOIN ui.inventory i JOIN i.itemList item WHERE item.expDate < CURRENT_DATE")
+    List<UserInventory> findAllByExpiredItemsInInventory();
 
 }
