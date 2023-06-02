4fd14ea3878ae92d8d3ae4e08a87115aee877249
YehyeokBang <qkddpgur318@gmail.com>
2023-06-02 09:14:46 +0900

    refactor: Replace URLConnection with RestTemplate (#23)

--- src/main/java/skhu/easysobi/barcode/service/BarcodeService.java
+++ src/main/java/skhu/easysobi/barcode/service/BarcodeService.java
@@ -5,12 +5,15 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import skhu.easysobi.barcode.dto.BarcodeDTO;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -19,38 +22,38 @@ public class BarcodeService {
     @Value("${barcode-api-key}")
     private String apiKey;
 
-    public BarcodeDTO.Response getFoodInfo (String barcode) {
-        // 식품명, 식품 유형, 유통/소비기한 정보
-        String name = "";
-        String type = "";
-        String expInfo = "";
-
+    public BarcodeDTO.Response getFoodInfo(String barcode) {
+        // RestTemplate 객체 생성
+        RestTemplate restTemplate = new RestTemplate();
+        // 바코드 API 요청 URL 설정
         String reqURL = "http://openapi.foodsafetykorea.go.kr/api/" + apiKey + "/C005/json/1/5/BAR_CD=" + barcode;
 
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
 
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line = "";
-            StringBuilder result = new StringBuilder();
+        // HttpEntity 생성, 헤더 포함
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
 
-            while ((line = br.readLine()) != null) {
-                result.append(line);
-            }
+        // RestTemplate을 이용해 GET 요청 후 응답 받기
+        ResponseEntity<String> responseEntity = restTemplate.exchange(reqURL, HttpMethod.GET, requestEntity, String.class);
 
-            // Gson 라이브러리로 JSON파싱
-            JsonElement element = JsonParser.parseString(result.toString()).getAsJsonObject();
-            JsonObject jsonObject = element.getAsJsonObject().get("C005").getAsJsonObject().get("row").getAsJsonArray().asList().get(0).getAsJsonObject();;
+        // 식품명, 식품 유형, 소비기한 정보를 저장할 변수 선언
+        String name, type, expInfo = "";
+
+        // 성공적인 응답인 경우
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            // 응답 본문(JSON)을 파싱하기 위한 JsonElement 객체 생성
+            JsonElement element = JsonParser.parseString(Objects.requireNonNull(responseEntity.getBody())).getAsJsonObject();
+
+            // JsonElement 객체에서 식품명, 식품 유형, 소비기한 정보 추출
+            JsonObject jsonObject = element.getAsJsonObject().get("C005").getAsJsonObject().get("row").getAsJsonArray().asList().get(0).getAsJsonObject();
             name = jsonObject.get("PRDLST_NM").getAsString();
             type = jsonObject.get("PRDLST_DCNM").getAsString();
             expInfo = jsonObject.get("POG_DAYCNT").getAsString();
-
-            br.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } else {
+            // 성공적인 응답이 아닐 경우 예외 발생
+            throw new IllegalStateException("식품 정보를 불러오는 중 오류 발생: " + responseEntity.getStatusCode());
         }
 
         return new BarcodeDTO.Response(name, type, expInfo, barcode);
