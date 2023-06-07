a550b1f066c5f4ade4eec7a1e27676dc2fa8adc6
YehyeokBang <qkddpgur318@gmail.com>
2023-06-07 16:49:55 +0900

    feat: Add CustomExceptionHandler (#24)

---
+++ src/main/java/skhu/easysobi/common/exception/CustomExceptionHandler.java
@@ -0,0 +1,15 @@
+package skhu.easysobi.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+}
---
+++ src/main/java/skhu/easysobi/common/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package skhu.easysobi.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력 값이 잘못되었습니다."),
+    INVALID_KAKAO_VALUE(HttpStatus.BAD_REQUEST, "잘못된 요청으로 카카오 서버의 응답을 받지 못했습니다."),
+    INVALID_BARCODE_VALUE(HttpStatus.BAD_REQUEST, "잘못된 요청으로 바코드 서버의 응답을 받지 못했습니다."),
+
+    /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
+    INVALID_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "인증 토큰이 유효하지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 유효하지 않습니다."),
+
+    /* 404 NOT_FOUND : 리소스를 찾을 수 없음 */
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자의 정보를 찾을 수 없습니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자의 이메일을 찾을 수 없습니다."),
+    NAME_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자의 이름(닉네임)을 찾을 수 없습니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
